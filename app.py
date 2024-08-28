c27e4e5572df8dfa09aeec86f059ed63e29d2033
Brandon <Brandonwookeanjin@gmail.com>
2024-08-28 22:25:58 +0800

    corrected an error to change the country_df merge to be a right-join instead of inner-join

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import json
 import pandas as pd
 
 restauranturl = "https://raw.githubusercontent.com/Papagoat/brain-assessment/main/restaurant_data.json"
-countrydf = pd.read_excel("Country-Code.xlsx")
-countrydf.columns = ["country_id", "country"]
+country_code_df = pd.read_excel("Country-Code.xlsx")
+country_code_df.columns = ["country_id", "country"]
 
 response = requests.get(restauranturl)
 data = response.json()
@@ -19,16 +19,15 @@ for row in data:
         row_count += 1
 
 restaurant_id_df = df["id"].to_frame()
-restaurant_id_df.columns = ["id"]
+restaurant_id_df.columns = ["restaurant_id"]
 
 restaurant_name_df = df["name"].to_frame()
 restaurant_name_df.columns = ['name']
 
 locations_df = df['location']
-country_id_df = locations_df.apply(lambda row: row["country_id"])
-country_id_df = country_id_df.to_frame()
+country_id_df = locations_df.apply(lambda row: row["country_id"]).to_frame()
 country_id_df.columns = ['country_id']
-country = pd.merge(countrydf, country_id_df, on = "country_id")
+country_df = pd.merge(country_code_df, country_id_df, on = "country_id", how = "right")
 
 city_df = locations_df.apply(lambda row: row['city']).to_frame()
 city_df.columns = ["city"]
@@ -42,3 +41,5 @@ aggregate_rating_df.columns = ["aggregate_rating"]
 
 cuisines_df = df["cuisines"].to_frame()
 cuisines_df.columns = ["cuisines"]
+
+
