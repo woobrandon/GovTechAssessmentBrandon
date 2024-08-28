a61737bd86d9e92cc9c1751b459c722f7f20b7c7
Brandon <Brandonwookeanjin@gmail.com>
2024-08-28 09:09:32 +0800

    created a pandas dataframe from JSON file

--- app.py
+++ app.py
@@ -7,4 +7,14 @@ restauranturl = "https://raw.githubusercontent.com/Papagoat/brain-assessment/mai
 response = requests.get(restauranturl)
 data = response.json()
 
-print(data[0])
+column_names = list(data[0]["restaurants"][0]['restaurant'].keys())
+df = pd.DataFrame(columns=column_names)
+row_count = 0
+for row in data:
+    for restaurant in row["restaurants"]:
+        temp = restaurant['restaurant']
+        temp["R"] = temp["R"]['res_id']
+        df.loc[row_count] = restaurant['restaurant']
+        row_count += 1
+
+print(df)
