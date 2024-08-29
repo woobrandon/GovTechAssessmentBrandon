06b5dabe414e45f459d958217f9e5223078ff11b
Brandon <Brandonwookeanjin@gmail.com>
2024-08-29 17:07:31 +0800

    create a function to filter the dataframe for the corrent dates

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import requests
-import json
 import pandas as pd
+from datetime import datetime
 
 restauranturl = "https://raw.githubusercontent.com/Papagoat/brain-assessment/main/restaurant_data.json"
 country_code_df = pd.read_excel("Country-Code.xlsx")
@@ -23,6 +23,7 @@ restaurant_id_df.columns = ["restaurant_id"]
 
 restaurant_name_df = df["name"].to_frame()
 restaurant_name_df.columns = ['name']
+temp = restaurant_name_df[restaurant_name_df['name'] == "Chili's Grill & Bar"]
 
 locations_df = df['location']
 country_id_df = locations_df.apply(lambda row: row["country_id"]).to_frame()
@@ -44,5 +45,22 @@ cuisines_df.columns = ["cuisines"]
 
 restaurants = pd.concat([restaurant_id_df, restaurant_name_df, country_df, city_df, rating_votes_df, aggregate_rating_df, cuisines_df], axis = 1)
 
+# restaurants.to_csv("restaurants.csv", index = False)
 
-restaurants.to_csv("restaurants.csv", index = False)
+def filter_event(row):
+    zomato_event = row["zomato_events"]
+    if type(zomato_event) == list:
+        target = datetime.strptime("2019-04", "%Y-%m")
+        event_data = zomato_event[0]["event"]
+        start_date = event_data["start_date"][:7]
+        start_date = datetime.strptime(start_date, "%Y-%m")
+        end_date = event_data["end_date"][:7]
+        end_date = datetime.strptime(end_date, "%Y-%m")
+        if start_date <= target and end_date >= target:
+            return True
+
+print()
+event_filtered_df = df.apply(lambda row: row if filter_event(row) else None, axis = 1)
+print(event_filtered_df.loc[29])
+print(datetime.strptime(df.loc[29]["zomato_events"][0]["event"]["start_date"][:7], "%Y-%m") <= datetime.strptime("2019-04", "%Y-%m"))
+print(df.loc[29]["zomato_events"])
