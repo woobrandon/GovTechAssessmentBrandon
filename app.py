541af48c7ea4670215c56f6a613561e13efdc80d
Brandon <Brandonwookeanjin@gmail.com>
2024-08-30 14:07:15 +0800

    add comments to immprove clarity

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 from datetime import datetime
 
-#importing the 2 datasets
+# importing the 2 datasets
 restauranturl = "https://raw.githubusercontent.com/Papagoat/brain-assessment/main/restaurant_data.json"
 country_code_df = pd.read_excel("Country-Code.xlsx")
 country_code_df.columns = ["country_id", "country"]
@@ -19,7 +19,7 @@ for row in data:
         df.loc[row_count] = restaurant['restaurant']
         row_count += 1
 
-#creating individual columns dataframe
+# creating individual columns dataframe
 def get_column(column, dataframe = df, nested = False):
     if not nested:
         column_df = dataframe[column].to_frame()
@@ -49,9 +49,10 @@ cuisines_df = get_column("cuisines")
 
 restaurants = pd.concat([restaurant_id_df, restaurant_name_df, country_df, city_df, rating_votes_df, aggregate_rating_df, cuisines_df], axis = 1)
 
+# exporting restaurants to csv
 restaurants.to_csv("restaurants.csv", index = False)
 
-
+# filtering events to check if they fall in April 2019
 def filter_event(row):
     zomato_event = row["zomato_events"]
     if type(zomato_event) == list:
@@ -69,6 +70,7 @@ filter_df = df.apply(lambda row: True if filter_event(row) else False, axis = 1)
 df2 = df[filter_df].reset_index(drop = True)
 zomato_events_df = df2["zomato_events"]
 
+# creating individual columns dataframe
 restaurant_id_df2 = restaurant_id_df[filter_df].reset_index(drop = True)
 
 restaurant_name_df2 = restaurant_name_df[filter_df].reset_index(drop = True)
@@ -89,13 +91,16 @@ start_date_df = zomato_events_filter("start_date")
 
 end_date_df = zomato_events_filter("end_date")
 
-restuarant_event_df = pd.concat([event_id_df, restaurant_id_df2, restaurant_name_df2, photo_url_df, event_title_df, start_date_df,end_date_df], axis = 1)
+restaurant_event_df = pd.concat([event_id_df, restaurant_id_df2, restaurant_name_df2, photo_url_df, event_title_df, start_date_df,end_date_df], axis = 1)
 
-restuarant_event_df.to_csv("restaurants_events.csv", index = False)
+# exporting restaurant_event_df to csv file
+restaurant_event_df.to_csv("restaurants_events.csv", index = False)
 
-rating_text_df = df['user_rating'].apply(lambda row: row["rating_text"])
+# merging aggregate_rating_df and rating_text_df
+rating_text_df = get_column("rating_text", user_rating_df, True)
 user_votes_ratings_df = pd.concat([aggregate_rating_df,rating_text_df], axis = 1)
 
+# getting the threshold values and printing to the terminal
 def threshold_filter(rating):
     rating_df = user_votes_ratings_df[user_votes_ratings_df["user_rating"] == rating]
     threshold_df = [min(rating_df["aggregate_rating"]),max(rating_df["aggregate_rating"])]
