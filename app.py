8c7135b9bc91d4a32d581c0aa9c3186b01b8025d
Brandon <Brandonwookeanjin@gmail.com>
2024-08-30 14:01:19 +0800

    changed get_column() to allow for nested data

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 from datetime import datetime
 
+#importing the 2 datasets
 restauranturl = "https://raw.githubusercontent.com/Papagoat/brain-assessment/main/restaurant_data.json"
 country_code_df = pd.read_excel("Country-Code.xlsx")
 country_code_df.columns = ["country_id", "country"]
@@ -9,6 +10,7 @@ country_code_df.columns = ["country_id", "country"]
 response = requests.get(restauranturl)
 data = response.json()
 
+# convorting the json file into a pandas dataframe
 column_names = list(data[0]["restaurants"][0]['restaurant'].keys())
 df = pd.DataFrame(columns=column_names)
 row_count = 0
@@ -17,38 +19,37 @@ for row in data:
         df.loc[row_count] = restaurant['restaurant']
         row_count += 1
 
-def get_column(column):
-    column_df = df[column].to_frame()
+#creating individual columns dataframe
+def get_column(column, dataframe = df, nested = False):
+    if not nested:
+        column_df = dataframe[column].to_frame()
+    else:
+        column_df = dataframe.apply(lambda row: row[column]).to_frame()
     column_df.columns = [column]
     return column_df
 
 restaurant_id_df = get_column("id")
-print(restaurant_id_df)
 
 restaurant_name_df = get_column("name")
 
 locations_df = df['location']
-country_id_df = locations_df.apply(lambda row: row["country_id"]).to_frame()
-country_id_df.columns = ['country_id']
+
+country_id_df = get_column("country_id", locations_df, True)
 country_df = pd.merge(country_code_df, country_id_df, on = "country_id", how = "right")
 
-city_df = locations_df.apply(lambda row: row['city']).to_frame()
-city_df.columns = ["city"]
+city_df = get_column("city", locations_df, True)
 
 user_rating_df = df['user_rating']
-rating_votes_df = user_rating_df.apply(lambda row: row["votes"]).to_frame()
-rating_votes_df.columns = ["votes"]
 
-aggregate_rating_df = user_rating_df.apply(lambda row: float(row["aggregate_rating"])).to_frame()
-aggregate_rating_df.columns = ["aggregate_rating"]
+rating_votes_df = get_column("votes", user_rating_df, True)
 
-cuisines_df = df["cuisines"].to_frame()
-cuisines_df.columns = ["cuisines"]
+aggregate_rating_df = get_column("aggregate_rating", user_rating_df, True)
+
+cuisines_df = get_column("cuisines")
 
 restaurants = pd.concat([restaurant_id_df, restaurant_name_df, country_df, city_df, rating_votes_df, aggregate_rating_df, cuisines_df], axis = 1)
 
 restaurants.to_csv("restaurants.csv", index = False)
-
 
 
 def filter_event(row):
